03217467685be1e13a3d787ea033386e987dc4a0
Rene Lüllau <Rene.luellau@mailbox.org>
2026-04-15 02:16:03 +0200

    feat: Add Album Cover setting to shuffle action
    Signed-off-by: Rene Lüllau <Rene.luellau@mailbox.org>

--- actions/shuffle.py
+++ actions/shuffle.py
@@ -36,11 +36,13 @@ class ShuffleAction(ActionBase):
             return
         if not self.backend.is_authed():
             icon_path = os.path.join(self.plugin_base.PATH, "assets", "icons8-spotify-no-auth-100.png")
+            self.set_media(media_path=icon_path, size=0.75)
         else:
             self.set_top_label(self.actionSettings.get_text(self.Texts.TOP))
             self.set_center_label(self.actionSettings.get_text(self.Texts.MIDDLE))
             self.set_bottom_label(self.actionSettings.get_text(self.Texts.BOTTOM))
 
+            icon_path = ""
             shuffle_mode = self.backend.get_shuffle_mode()
             if shuffle_mode == True:
                 icon_path = os.path.join(self.plugin_base.PATH, "assets", "icons8-shuffle-100.png")
@@ -48,7 +50,12 @@ class ShuffleAction(ActionBase):
                 icon_path = os.path.join(self.plugin_base.PATH, "assets", "icons8-shuffle-off-100.png")
             else:
                 icon_path = os.path.join(self.plugin_base.PATH, "assets", "icons8-shuffle-no-music-100.png")
-        self.set_media(media_path=icon_path, size=0.75)
+
+            btn_img = self.actionSettings.get_media(icon_path=icon_path)
+            if btn_img is not None:
+                self.set_media(image=btn_img)
+            else:
+                self.set_media(None)
 
     def on_key_down(self) -> None:
         # Toggle shuffle mode
