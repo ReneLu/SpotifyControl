078c0fd138b3c2f128fb98c65700ee8ca1b36263
Rene Lüllau <Rene.luellau@mailbox.org>
2026-04-15 02:15:41 +0200

    feat: Add Album Cover setting to repeat action
    Signed-off-by: Rene Lüllau <Rene.luellau@mailbox.org>

--- actions/repeat.py
+++ actions/repeat.py
@@ -36,11 +36,13 @@ class RepeatAction(ActionBase):
             return
         if not self.backend.is_authed():
             icon_path = os.path.join(self.plugin_base.PATH, "assets", "icons8-spotify-no-auth-100.png")
+            self.set_media(media_path=icon_path, size=0.75)
         else:
             self.set_top_label(self.actionSettings.get_text(self.Texts.TOP))
             self.set_center_label(self.actionSettings.get_text(self.Texts.MIDDLE))
             self.set_bottom_label(self.actionSettings.get_text(self.Texts.BOTTOM))
 
+            icon_path = ""
             repeat_state = self.backend.get_current_repeat_state()
             if repeat_state == "off":
                 icon_path = os.path.join(self.plugin_base.PATH, "assets", "icons8-repeat-off-100.png")
@@ -50,7 +52,12 @@ class RepeatAction(ActionBase):
                 icon_path = os.path.join(self.plugin_base.PATH, "assets", "icons8-repeat-1-100.png")
             else:
                 icon_path = os.path.join(self.plugin_base.PATH, "assets", "icons8-repeat-no-music-100.png")
-        self.set_media(media_path=icon_path, size=0.75)
+
+            btn_img = self.actionSettings.get_media(icon_path=icon_path)
+            if btn_img is not None:
+                self.set_media(image=btn_img)
+            else:
+                self.set_media(None)
 
     def on_key_down(self) -> None:
         if self.backend.is_authed():
