0a6c0744feac8380a6e5c0b0a2cbfb4ae3a31fb3
Rene Lüllau <Rene.luellau@mailbox.org>
2026-04-15 02:17:05 +0200

    feat: Add Album Cover setting to volume mute action
    Signed-off-by: Rene Lüllau <Rene.luellau@mailbox.org>

--- actions/vol_mute.py
+++ actions/vol_mute.py
@@ -37,11 +37,13 @@ class VolMuteAction(ActionBase):
             return
         if not self.backend.is_authed():
             icon_path = os.path.join(self.plugin_base.PATH, "assets", "icons8-spotify-no-auth-100.png")
+            self.set_media(media_path=icon_path, size=0.75)
         else:
             self.set_top_label(self.actionSettings.get_text(self.Texts.TOP))
             self.set_center_label(self.actionSettings.get_text(self.Texts.MIDDLE))
             self.set_bottom_label(self.actionSettings.get_text(self.Texts.BOTTOM))
 
+            icon_path = ""
             volume = self.backend.get_volume()
 
             # Set icon
@@ -59,7 +61,11 @@ class VolMuteAction(ActionBase):
                 # Set icon to no sound available
                 icon_path = os.path.join(self.plugin_base.PATH, "assets", "icons8-no-sound-100.png")
 
-        self.set_media(media_path=icon_path, size=0.75)
+            btn_img = self.actionSettings.get_media(icon_path=icon_path)
+            if btn_img is not None:
+                self.set_media(image=btn_img)
+            else:
+                self.set_media(None)
 
     def on_key_down(self) -> None:
         # Toggle mute
